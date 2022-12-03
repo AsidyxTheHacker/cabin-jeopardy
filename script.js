c931ffad8de82d160123a2a2fb6a57915b821b62
Adam <adammiller.2000nj@gmail.com>
2022-12-03 15:16:57 -0500

    added more answers

--- script.js
+++ script.js
@@ -1,105 +1,196 @@
 let popup1001 = document.getElementById("popup")
+let ans = document.getElementById("ans")
 function openPopup() {
     popup1001.classList.add("open-pop");
 }
 function closePopup() {
     popup1001.classList.remove("open-pop");
 }
+function displayAns() {
+    ans.classList.add("answer");
+}
+function removeAns() {
+    ans.classList.remove("answer");
+}
 //==================================================
 let popup1002 = document.getElementById("popup2")
+let ans2 = document.getElementById("ans2")
 function openPopup2() {
     popup1002.classList.add("open-pop2");
 }
 function closePopup2() {
     popup1002.classList.remove("open-pop2");
 }
+function displayAns2() {
+    ans2.classList.add("answer2");
+}
+function removeAns2() {
+    ans2.classList.remove("answer2");
+}
 //==================================================
 let popup1003 = document.getElementById("popup3")
+let ans3 = document.getElementById("ans3")
 function openPopup3() {
     popup1003.classList.add("open-pop3");
 }
 function closePopup3() {
     popup1003.classList.remove("open-pop3");
 }
+function displayAns3() {
+    ans3.classList.add("answer3");
+}
+function removeAns3() {
+    ans3.classList.remove("answer3");
+}
 //==================================================
 let popup1004 = document.getElementById("popup4")
+let ans4 = document.getElementById("ans4")
 function openPopup4() {
     popup1004.classList.add("open-pop4");
 }
 function closePopup4() {
     popup1004.classList.remove("open-pop4");
 }
+function displayAns4() {
+    ans4.classList.add("answer4");
+}
+function removeAns4() {
+    ans4.classList.remove("answer4");
+}
 //==================================================
 let popup1005 = document.getElementById("popup5")
+let ans5 = document.getElementById("ans5")
 function openPopup5() {
     popup1005.classList.add("open-pop5");
 }
 function closePopup5() {
     popup1005.classList.remove("open-pop5");
 }
+function displayAns5() {
+    ans5.classList.add("answer5");
+}
+function removeAns5() {
+    ans5.classList.remove("answer5");
+}
 //==================================================
 let popup2001 = document.getElementById("popup21")
+let ans6 = document.getElementById("ans6")
 function openPopup21() {
     popup2001.classList.add("open-pop21");
 }
 function closePopup21() {
     popup2001.classList.remove("open-pop21");
 }
+function displayAns6() {
+    ans6.classList.add("answer6");
+}
+function removeAns6() {
+    ans6.classList.remove("answer6");
+}
 //==================================================
 let popup2002 = document.getElementById("popup22")
+let ans7 = document.getElementById("ans7")
 function openPopup22() {
     popup2002.classList.add("open-pop22");
 }
 function closePopup22() {
     popup2002.classList.remove("open-pop22");
 }
+function displayAns7() {
+    ans7.classList.add("answer7");
+}
+function removeAns7() {
+    ans7.classList.remove("answer7");
+}
 //==================================================
 let popup2003 = document.getElementById("popup23")
+let ans8 = document.getElementById("ans8")
 function openPopup23() {
     popup2003.classList.add("open-pop23");
 }
 function closePopup23() {
     popup2003.classList.remove("open-pop23");
 }
+function displayAns8() {
+    ans8.classList.add("answer8");
+}
+function removeAns8() {
+    ans8.classList.remove("answer8");
+}
 //==================================================
 let popup2004 = document.getElementById("popup24")
+let ans9 = document.getElementById("ans9")
 function openPopup24() {
     popup2004.classList.add("open-pop24");
 }
 function closePopup24() {
     popup2004.classList.remove("open-pop24");
 }
+function displayAns9() {
+    ans9.classList.add("answer9");
+}
+function removeAns9() {
+    ans9.classList.remove("answer9");
+}
 //==================================================
 let popup2005 = document.getElementById("popup25")
+let ans10 = document.getElementById("ans10")
 function openPopup25() {
     popup2005.classList.add("open-pop25");
 }
 function closePopup25() {
     popup2005.classList.remove("open-pop25");
 }
+function displayAns10() {
+    ans10.classList.add("answer10");
+}
+function removeAns10() {
+    ans10.classList.remove("answer10");
+}
 //==================================================
 let popup3001 = document.getElementById("popup31")
+let ans11 = document.getElementById("ans11")
 function openPopup31() {
     popup3001.classList.add("open-pop31");
 }
 function closePopup31() {
     popup3001.classList.remove("open-pop31");
 }
+function displayAns11() {
+    ans11.classList.add("answer11");
+}
+function removeAns11() {
+    ans11.classList.remove("answer11");
+}
 //==================================================
 let popup3002 = document.getElementById("popup32")
+let ans12 = document.getElementById("ans12")
 function openPopup32() {
     popup3002.classList.add("open-pop32");
 }
 function closePopup32() {
     popup3002.classList.remove("open-pop32");
 }
+function displayAns12() {
+    ans12.classList.add("answer12");
+}
+function removeAns12() {
+    ans12.classList.remove("answer12");
+}
 //==================================================
 let popup3003 = document.getElementById("popup33")
+let ans13 = document.getElementById("ans13")
 function openPopup33() {
     popup3003.classList.add("open-pop33");
 }
 function closePopup33() {
     popup3003.classList.remove("open-pop33");
+}
+function displayAns13() {
+    ans13.classList.add("answer13");
+}
+function removeAns13() {
+    ans13.classList.remove("answer13");
 }
 //==================================================
 let popup3004 = document.getElementById("popup34")
@@ -198,44 +289,3 @@ function closePopup55() {
     popup5005.classList.remove("open-pop55");
 }
 //==================================================
-//QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
-//==================================================
-let ans = document.getElementById("ans")
-function displayAns() {
-    ans.classList.add("answer");
-}
-function removeAns() {
-    ans.classList.remove("answer");
-}
-//==================================================
-let ans2 = document.getElementById("ans2")
-function displayAns2() {
-    ans2.classList.add("answer2");
-}
-function removeAns2() {
-    ans2.classList.remove("answer2");
-}
-//==================================================
-let ans3 = document.getElementById("ans3")
-function displayAns3() {
-    ans3.classList.add("answer3");
-}
-function removeAns3() {
-    ans3.classList.remove("answer3");
-}
-//==================================================
-let ans4 = document.getElementById("ans4")
-function displayAns4() {
-    ans4.classList.add("answer4");
-}
-function removeAns4() {
-    ans4.classList.remove("answer4");
-}
-//==================================================
-let ans5 = document.getElementById("ans5")
-function displayAns5() {
-    ans5.classList.add("answer5");
-}
-function removeAns5() {
-    ans5.classList.remove("answer5");
-}
